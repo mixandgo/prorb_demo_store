5fc145abd37640aea2090e9d4c210f278801f4ab
Cezar Halmagean <cezar@mixandgo.com>
2024-03-19 10:10:49 +0200

    Add /admin section

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+      resources :categories
+      resources :products
+      resources :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+      root to: "categories#index"
+    end
   resources :categories, only: %i(index show)
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
